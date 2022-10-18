f129945e3feb46af688f1d1a85fa87f11405807d
Josias Buday Developer <josiasbuday@gmail.com>
2022-10-18 01:29:24 -0300

    Correcting bug in Module 3 - WebAPI Restful\middlewares\authentication-middleware.js

--- Module 3 - WebAPI Restful/middlewares/authentication-middleware.js
+++ Module 3 - WebAPI Restful/middlewares/authentication-middleware.js
@@ -1,11 +1,19 @@
 const keyModel = require('../models/keyModel');
+const fs = require('fs');
+const path = require('path');
+const FILE_PATH = path.join(__dirname, '..', 'data', 'keys.json');
+let keys = fs.readFileSync(FILE_PATH, {encoding: 'utf8'});
+keys = JSON.parse(keys);
+const key = keys[0].key;
 
 function authentication(req, res, next) {
-  const key = req.headers['authorization'];
-  const apiKey = keyModel.findKey(key.replace('ApiKey',''));
-
-  if (apiKey && apiKey.enabled) {
-    return next();
+  if (req.headers['authorization'] && req.headers['authorization'] == key) {
+    const key = req.headers['authorization'];
+    const apiKey = keyModel.findKey(key.replace('ApiKey',''));
+  
+    if (apiKey && apiKey.enabled) {
+      return next();
+    }
   }
   else {
     // status 401: unauthorized
